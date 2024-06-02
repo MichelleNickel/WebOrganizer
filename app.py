badd04572d4b982269e986069d32b6ce9215c847
Michelle Nickel <michelle.nickel96@gmx.de>
2024-06-02 14:44:18 +0200

    Create draft of to dos

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Initialize database
 connecc = sqlite3.connect('test.db', check_same_thread=False, autocommit=True)
 
+# Create User table
 # add email TEXT NOT NULL in users ? 
 connecc.execute("""
         CREATE TABLE IF NOT EXISTS Users (
@@ -30,15 +31,37 @@ connecc.execute("""
                 username TEXT NOT NULL UNIQUE,
                 password_hash TEXT NOT NULL  
         ) """)
+# Create Layout_prefs table
 connecc.execute("""
         CREATE TABLE IF NOT EXISTS Layout_prefs (
                 user_id INTEGER NOT NULL,
                 element_name TEXT NOT NULL,
                 display_order INTEGER NOT NULL,
-                PRIMARY KEY (user_id, element_name)
+                PRIMARY KEY (user_id, element_name),
                 FOREIGN KEY (user_id) REFERENCES Users(user_id)
         ) """) # maybe no id, but rather user_id and element_name as key? (id INTEGER PRIMARY KEY AUTOINCREMENT)
 #connecc.execute("CREATE IF NOT EXIST UNIQUE INDEX username ON users (username);")
+# Create Todo table (done task 0=false, 1=true)
+connecc.execute("""
+        CREATE TABLE IF NOT EXISTS Todo (
+                user_id INTEGER NOT NULL,
+                title TEXT NOT NULL,
+                done INTEGER NOT NULL CHECK (outstanding IN (0,1)),
+                PRIMARY KEY (user_id, title),
+                FOREIGN KEY (user_id) REFERENCES Users(user_id)
+        ) """)
+# Create Shopping list
+# e.g. unit=1KG and amount=3
+connecc.execute("""
+        CREATE TABLE IF NOT EXISTS Shopping_list (
+                user_id INTEGER NOT NULL,
+                item_name TEXT NOT NULL,
+                item_amount INTEGER NOT NULL,
+                item_unit TEXT,
+                outstanding INTEGER NOT NULL CHECK (outstanding IN (0,1)),
+                PRIMARY KEY (user_id, title),
+                FOREIGN KEY (user_id) REFERENCES Users(user_id)
+        ) """)
 
 
 
@@ -53,6 +76,14 @@ dbCursor = connecc.cursor()
 # ---------------- GLOBAL VARIABLES
 
 WIDGETLIST = ["toDo", "week", "month", "year", "projects", "shopping"]
+
+
+# ---------------- TESTING PURPOSE ROUTE 
+
+@app.route("/test")
+def testRoute():
+    redirect("/todo")
+
 
 # ---------------- APP ROUTES
 
@@ -322,6 +353,22 @@ def reset_layout():
         redirect("/")
 
 
+# To Dos 
+@app.route("/todo", methods=["GET", "POST"])
+def todo():
+    if "user_id" in session:
+        user_id = session["user_id"]
+
+        # Fetch user's to do points from the database
+        currentToDos = get_current_toDos(user_id)
+        oldToDos = get_old_toDos(user_id)
+
+        return render_template("setLayout.html", currentToDos=currentToDos, oldToDos=oldToDos)
+    else:
+        # If there is no user logged in, tell them to log in first
+        flash("You need to be logged in to see your to dos :).", 'danger')
+        redirect("/")
+
 
 # ---------------- HELPER METHODS
 
@@ -347,7 +394,18 @@ def check_password_validity(password):
 # Deleting the saved layout preferences of the current user
 def delete_user_lprefs(user_id):
     dbCursor.execute("DELETE FROM Layout_prefs WHERE user_id = ?", user_id)
-    return
+    return dbCursor.fetchall()
+
+# Getting the current (not done yet) to dos for the given user_id
+def get_current_toDos(user_id):
+    dbCursor.execute("SELECT title FROM Todo WHERE user_id = ? WHERE done = 0", user_id)
+    return dbCursor.fetchall()
+
+# Getting the old (already done but not deleted yet to dos for the given user)
+def get_old_toDos(user_id):
+    dbCursor.execute("SELECT title FROM Todo WHERE user_id = ? WHERE done = 1", user_id)
+    return dbCursor.fetchall()
+
 
 def get_user_lprefs(user_id):
     # Databasequery to fetch the user preferences
@@ -365,204 +423,3 @@ def save_user_lprefs(user_id, element_name, display_order):
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#@app.route("/register", methods=["GET", "POST"])
-#def register(): 
-#    # Define a constant variable for register.html site
-#    REGISTER_PAGE = "register.html"
-
-    # User reached the site via get request
-#    if request.method == "GET":
-#        return render_template(REGISTER_PAGE)
-    
-#    # User reached the site via post request
-#    else:
-#        # Ensure user typed in a username
-#        if not request.form.get("username"):
-#            # Flash error message on top of site
-#            flash("Please enter a username.", 'danger')
- #           return render_template(REGISTER_PAGE)
-        # Ensure user typed in a password and repeated it
-#        elif not request.form.get("password") or not request.form.get("confirmPassword"):
-#            # Return error message on top of site
-#            flash("Please enter a pasword and repeat it.", 'danger')
-#            return render_template(REGISTER_PAGE)
-#        # Ensure the password was repeated correctly
-#        elif request.form.get("password") != request.form.get("confirmPassword"):
-#            # Return yet another error message.
-#            flash("Please ensure that the passwords match.", 'danger')
-#            return render_template(REGISTER_PAGE)
-
-        # Assign the values to variables
-#        username = request.form.get("username")
-#        password = request.form.get("password")
-
-        # Check if user is already saved in database
-#        dbCursor.execute("SELECT * FROM Users WHERE username = ?", [username])
-#        if len(dbCursor.fetchall()) != 0:
-#            # return error message, username exists already.
-#            flash("This username already exists.", 'danger')
-#            return render_template(REGISTER_PAGE)
-
-        # Ensure password is conform to rules
- #       if not check_password_validity(password):
- #           # return errormessage, password not conform to rules
- #           flash("Your password does not meet our requirements.", 'danger')
-#            return render_template(REGISTER_PAGE)
-#        else:
-#            # Create a hash value of the password
-#            passwordHash = generate_password_hash(password)
-
-        # Insert userdata into the db
-#        dbCursor.execute("""INSERT INTO Users (username, password_hash) 
-#        VALUES (?,?)""", (username, passwordHash))
-
-        # Query the database again
-#        dbCursor.execute("SELECT * FROM users WHERE username = ?", [username])
-
-        # Check out old user, if someone is still logged in.
-#        if "user_id" in session:
-#            session.clear()
-
-        # Log user in
-#        session["user_id"] = dbCursor.fetchone()[0]
-#        # Tell the user that they are logged in.
-#        flash("You are now registered and logged in. :)", 'success')
-#        return redirect("/")
-    
-    
-# Check the validity of the password
-#def check_password_validity(password):
-#    symbols = '!?-+'
-    # Ensure the password is at least 8 characters long
-#    if (len(password) >= 8 and
-        # Ensure the password contains atleast one uppercase letter
-#            any(c.isupper() for c in password) and
-        # Ensure the password contains atleast one lowercase letter
-#            any(c.islower() for c in password) and
-        # Ensure the password contains atleast one number
-#            any(c.isdigit() for c in password) and
-        # Ensure the password contains atleast one symbol (?!-+)
- #           any(c in symbols for c in password) and
-        # Ensure that all of the characters are either a symbol or alphanums
-#            all(c.isalnum() or c in symbols for c in password)):
-#        return True
-#    else:
-#        return False
-    
-
-#@app.route("/login", methods=["GET", "POST"])
-#def login():
- #   LOGIN_PAGE = "login.html"
-
- #   if request.method == "GET":
- #       return render_template(LOGIN_PAGE)
- #   else:
-        # Ensure user typed in a username
- #       if not request.form.get("username"):
-            # Flash error message on top of site
- #           flash("Please enter your username.", 'danger')
- #           return render_template(LOGIN_PAGE)
- #       # Ensure user typed in a password
- #       elif not request.form.get("password"):
- #           # Return error message on top of site
- #           flash("Please enter your password.", 'danger')
- #           return render_template(LOGIN_PAGE)
-
-        # Assign variables
-#        username = request.form.get("username")
-#        password = request.form.get("password")
-#
-        # Check if user exists
-#        dbCursor.execute("SELECT * FROM Users WHERE username = ?", [username])
-#        if len(dbCursor.fetchall()) != 1:
-            # Return error message, user does not exist yet.
-#            flash("You do not have an account here. Maybe register first?", 'danger')
-#            return render_template(LOGIN_PAGE)
-        
-        # Get password hash from database
- #       dbCursor.execute("SELECT password_hash FROM Users WHERE username = ?", [username])
- #       savedHash = dbCursor.fetchone()[0]
-
-        # Compare password hash from db to typed in password by user
- #       if not check_password_hash(savedHash, password):
-            # If not equal, let the user know and do not log them in
- #           flash("Wrong password. Try again pls, you got this ;).", 'danger')
- #           return render_template(LOGIN_PAGE)
-          
-        # Get user_id from database
- #       dbCursor.execute("SELECT user_id FROM Users WHERE username = ?", [username])
- #       userID = dbCursor.fetchone()[0]
-    
-        # Save user_id in session (log them in)
- #       session["user_id"] = userID
- #       flash("You are now logged in. Hello :)", 'success')
- #       return redirect("/")
-
-#@app.route("/logout", methods=["GET", "POST"])
-#def logout():
-    # If user reached this site via GET
-#    if request.method == "GET":
-        # Show logout page
-#        return render_template("logout.html")
-    # If user reached this site via POST
-#    else:
-        # Clear the session (log the user out)
-#        session.clear()
-#        flash("You are now logged out. Goodbye :)", 'success')
-#        return redirect("/")
-    
-
-
-
-# Commit the changes to the database.
-#connecc.commit()
-
-# Close the db connection if not needed anymore
-# connecc.close()
-
-
-#if __name__ == "__main__":
-#    app.run(debug=True)
