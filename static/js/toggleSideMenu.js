a3aa65c06f616d942b49ab01fd6062454b7e9f54
Michelle Nickel <michelle.nickel96@gmx.de>
2024-07-14 14:29:16 +0200

    Try to animate the sidebar sliding in and out

---
+++ static/js/toggleSideMenu.js
@@ -0,0 +1,21 @@
+function toggleSideMenu() {
+    var list = document.getElementById("sideMenuList");
+    if (list.style.display === "none") {
+        list.style.display = "block";
+        window.setTimeout(function(){
+            list.style.opacity = 1;
+            list.style.transform = 'scale(1)';
+          },0);
+    } else {
+        list.style.opacity = 0;
+        list.style.transform = 'scale(0)';
+        window.setTimeout(function(){
+            list.style.display = 'none';
+        },0); // timed to match animation-duration
+    }
+}
+
+/**
+ * not scale, but maybe opacity and moving from left to right? 
+ * 
+ */
